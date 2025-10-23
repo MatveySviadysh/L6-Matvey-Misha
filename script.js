1b6ce885f2ac5da8e8d08af4bd97e7e72483ae88
matvey <mkomp06@gmail.com>
2025-10-23 17:03:43 +0300

    upgrade script.js

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('SPA Application started!');
     
+    if (typeof router !== 'undefined') {
+        console.log('Router initialized');
+    }
+    
+    setupFormEventListeners();
 });
+
+function setupFormEventListeners() {
+    document.removeEventListener('click', handleFormClick);
+    document.addEventListener('click', handleFormClick);
+}
+
+function handleFormClick(event) {
+    if (event.target && event.target.closest('#addUserForm')) {
+        const form = event.target.closest('#addUserForm');
+        if (form && !form.hasAttribute('data-listener-added')) {
+            form.addEventListener('submit', handleAddUserSubmit);
+            form.setAttribute('data-listener-added', 'true');
+        }
+    }
+    
+    if (event.target && event.target.closest('#addTodoForm')) {
+        const form = event.target.closest('#addTodoForm');
+        if (form && !form.hasAttribute('data-listener-added')) {
+            form.addEventListener('submit', handleAddTodoSubmit);
+            form.setAttribute('data-listener-added', 'true');
+        }
+    }
+}
