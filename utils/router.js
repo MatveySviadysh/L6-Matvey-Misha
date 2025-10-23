fab0ead2ff74ae52ee90393911923a5cfd356517
matvey <mkomp06@gmail.com>
2025-10-23 17:03:57 +0300

    new routers

--- utils/router.js
+++ utils/router.js
@@ -8,34 +8,28 @@ class Router {
     }
 
     init() {
-        // Обработчик изменения хэша
         window.addEventListener('hashchange', () => {
             this.handleRouteChange();
         });
 
-        // Обработчик загрузки страницы
         window.addEventListener('load', () => {
             this.handleRouteChange();
         });
     }
 
-    // Добавление маршрута
     addRoute(hash, callback) {
         this.routes[hash] = callback;
     }
 
-    // Обработчик изменения маршрута
     async handleRouteChange() {
         this.currentHash = window.location.hash || '#users';
         
         try {
-            // Рендерим хедер
             if (typeof renderHeader === 'function') {
                 this.appContainer.innerHTML = '';
                 this.appContainer.appendChild(renderHeader());
             }
 
-            // Рендерим хлебные крошки
             if (typeof renderBreadcrumbs === 'function') {
                 const breadcrumbsContainer = document.createElement('div');
                 breadcrumbsContainer.className = 'breadcrumbs-container';
@@ -43,7 +37,6 @@ class Router {
                 this.appContainer.appendChild(breadcrumbsContainer);
             }
 
-            // Рендерим контент в зависимости от маршрута
             const contentContainer = document.createElement('div');
             contentContainer.className = 'content-container';
             
@@ -54,19 +47,19 @@ class Router {
                     }
                     break;
                 
-                case '#users#todos':
+                case '#todos':
                     if (typeof renderTodos === 'function') {
                         contentContainer.appendChild(await renderTodos());
                     }
                     break;
                 
-                case '#users#posts':
+                case '#posts':
                     if (typeof renderPosts === 'function') {
                         contentContainer.appendChild(await renderPosts());
                     }
                     break;
                 
-                case '#users#posts#comments':
+                case '#comments':
                     if (typeof renderComments === 'function') {
                         contentContainer.appendChild(await renderComments());
                     }
@@ -84,16 +77,13 @@ class Router {
         }
     }
 
-    // Навигация
     navigate(hash) {
         window.location.hash = hash;
     }
 
-    // Получение текущего маршрута
     getCurrentRoute() {
         return this.currentHash;
     }
 }
 
-// Создаем экземпляр роутера
 const router = new Router();
